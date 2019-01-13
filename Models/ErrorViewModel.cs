642f860cd4ee66769dc2b79f5e024a613ba7f262
Stephen Maher <srqcoder@gmail.com>
2019-01-13 07:43:51 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VS2019WebApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
